450eeceb00d9e2fe12022dd3934e7c985f6f3c1f
javier <javierclavijog@gmail.com>
2021-08-09 14:52:30 +0200

    Add exercises 8.8, 8.9

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react'
+import {useQuery} from "@apollo/client";
+import {ALL_AUTHORS} from "../queries";
+
+const Authors = (props) => {
+    const result = useQuery(ALL_AUTHORS)
+    const [authors, setAuthors] = useState([])
+
+    useEffect(() => {
+        if (result.data) {
+            setAuthors(result.data.allAuthors)
+        }
+    }, [result])
+
+    if (result.loading) {
+        return <div>Loading...</div>
+    }
+
+    if (!props.show) {
+        return null
+    }
+
+
+    return (
+        <div>
+            <h2>authors</h2>
+            <table>
+                <tbody>
+                <tr>
+                    <th></th>
+                    <th>
+                        born
+                    </th>
+                    <th>
+                        books
+                    </th>
+                </tr>
+                {authors.map(a =>
+                    <tr key={a.name}>
+                        <td>{a.name}</td>
+                        <td>{a.born}</td>
+                        <td>{a.bookCount}</td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default Authors
