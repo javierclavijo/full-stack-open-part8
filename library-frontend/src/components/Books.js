9faf4569eeb9759e385191fb57e71ddb9369e2c2
javier <javierclavijog@gmail.com>
2021-08-19 14:58:37 +0200

    Add exercise 8.19

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -3,49 +3,66 @@ import {useQuery} from "@apollo/client";
 import {ALL_BOOKS} from "../queries";
 
 const Books = (props) => {
-  const result = useQuery(ALL_BOOKS)
-  const [books, setBooks] = useState([])
+    const result = useQuery(ALL_BOOKS)
+    const [books, setBooks] = useState([])
+    const [filter, setFilter] = useState('')
+    const [genres, setGenres] = useState([])
 
-  useEffect(() => {
-    if (result.data) {
-      setBooks(result.data.allBooks)
+    useEffect(() => {
+        if (result.data) {
+            setBooks(result.data.allBooks)
+            setGenres(Array.from(new Set(result.data.allBooks.flatMap(b => b.genres))))
+        }
+    }, [result])
+
+    if (result.loading) {
+        return <div>Loading...</div>
     }
-  }, [result])
 
-  if (result.loading) {
-    return <div>Loading...</div>
-  }
+    if (!props.show) {
+        return null
+    }
 
-  if (!props.show) {
-    return null
-  }
+    return (
+        <div>
+            <h2>books</h2>
 
-  return (
-    <div>
-      <h2>books</h2>
+            <table>
+                <tbody>
+                <tr>
+                    <th></th>
+                    <th>
+                        author
+                    </th>
+                    <th>
+                        published
+                    </th>
+                </tr>
+                {books.map(a =>
+                    !filter || a.genres.includes(filter)
+                        ? <tr key={a.title}>
+                            <td>{a.title}</td>
+                            <td>{a.author.name}</td>
+                            <td>{a.published}</td>
+                        </tr>
+                        : null
+                )}
+                </tbody>
+            </table>
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
+            <div>
+                <h4>Filter by genre</h4>
+                <select
+                    value={filter}
+                    onChange={(e) => setFilter(e.target.value)}>
+                    <option value="">No filter</option>
+                    {(genres.map(genre =>
+                        <option value={genre}>{genre}</option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    )
 }
 
 export default Books
